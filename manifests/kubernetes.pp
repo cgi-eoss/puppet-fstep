03fddfdfe51f8477ab3d4675d2ddd85a2962f7e7
Fabio D'Andria <fabio.dandria@cgi.com>
2017-06-29 11:44:49 +0200

    add etcd cluster installation

--- manifests/kubernetes.pp
+++ manifests/kubernetes.pp
@@ -2,6 +2,9 @@ class ftep::kubernetes(){
 
   require ::ftep::globals
   require ::epel
+  require ::etcd
+  require ::kubernetes::master
+  require ::kubernetes::node
 
   contain ::ftep::kubernetes::master
   contain ::ftep::kubernetes::worker
--- manifests/kubernetes/master.pp
+++ manifests/kubernetes/master.pp
@@ -1,5 +1,5 @@
 class ftep::kubernetes::master(
-  $kubernetes_master_ip = 'localhost',
+  $kubernetes_master_ip = '192.168.101.13',
 ) {
 
   require ::ftep::globals
@@ -7,5 +7,25 @@ class ftep::kubernetes::master(
 
   $real_kubernetes_master_ip = pick($kubernetes_master_ip, $ftep::globals::kubernetes_master_ip)
 
+  include ::kubernetes::client
+
+  class { 'etcd':
+    listen_client_urls          => 'http://0.0.0.0:2379',
+    advertise_client_urls       => "http://${real_kubernetes_master_ip}:2379,http://127.0.0.1:2379",
+    listen_peer_urls            => 'http://0.0.0.0:2380',
+    initial_advertise_peer_urls => "http://${real_kubernetes_master_ip}:2380,http://127.0.0.1:2379",
+    initial_cluster             => [
+      "${::hostname}=http://${::fqdn}:2380",
+      ],
+  } ->
+
+  class { 'kubernetes::master::apiserver':
+    ensure => running,
+    allow_privileged => true,
+    service_cluster_ip_range => '192.168.5.0/24'
+  } ->
+  class { 'kubernetes::master::scheduler':
+    master => "${real_kubernetes_master_ip}:8080",
+  }
 }
 
