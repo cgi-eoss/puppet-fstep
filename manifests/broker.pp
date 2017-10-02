2b3de20c2933412836c2e051e15c3e06c49db591
Fabio D'Andria <fabio.dandria@cgi.com>
2017-10-02 15:40:23 +0200

    install activemq from source

--- manifests/broker.pp
+++ manifests/broker.pp
@@ -4,8 +4,9 @@ class fstep::broker (){
   require ::epel
 
   class { 'activemq':
+    install             => 'source',
     version             => '5.14.5',
-    install_dependencies => false,
+    install_dependencies => true,
     install_destination => '/opt',      # Default value
     create_user         => true,        # Default value
     process_user        => 'activemq',  # Default value
