ac78dad6fcb0a8e15e283b39053ff8ebc0a5504d
Antonio Cuomo <antonio.cuomo@cgi.com>
2018-03-15 19:02:26 +0100

    Puppetize Angular router configuration for fstep-ui

--- manifests/ui.pp
+++ manifests/ui.pp
@@ -11,11 +11,27 @@ class fstep::ui (
     name   => 'fs-tep-ui',
     tag    => 'fstep',
   })
+  
+    $directories = [
+      {
+        'provider'   => 'location',
+        'path'       => '/analyst',
+		'custom_fragment' => ' RewriteEngine On
+		# If an existing asset or directory is requested go to it as it is
+		RewriteCond %{DOCUMENT_ROOT}%{REQUEST_URI} -f [OR]
+		RewriteCond %{DOCUMENT_ROOT}%{REQUEST_URI} -d
+		RewriteRule ^ - [L]
+		# If the requested resource does not exist, use index.html
+		RewriteRule ^ /analyst/index.html'
+		}
+    ]
 
   ::apache::vhost { 'fstep-ui':
     port       => '80',
     servername => 'fstep-ui',
     docroot    => $app_path,
+    directories => $directories
   }
 
 }
+
